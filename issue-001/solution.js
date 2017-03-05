597426aa593c1743b226e8ff425dfcec9576310a
Mariam Mghenyi <mchanya75@gmail.com>
2017-03-05 20:03:06 +0300

    solution: data-type-check

--- issue-001/solution.js
+++ issue-001/solution.js
@@ -1,3 +1,36 @@
 function dataTypeCheck(a) {
     // implement your solution here
+    type = typeof(a)
+    if (type === 'number') {
+    	//check for integer datatypes
+    	if ( a % 1 === 0 ){
+    		return a / 2
+    	}
+    	//check for float datatypes
+    	else if (parseFloat(a) == a) {
+    		return a * 2;
+    	} 
+    	//check for NaN datatypes
+    	else {
+    		return 0;
+    	}
+    }
+    else if (type === 'string') {
+    	//check for strings that can be type-cast
+    	if (+a) {
+    		return +a;
+    	}
+    	//check for other string types
+    	else {
+    		return "hello " + a;
+    	}
+    }
+    //check for null data types
+    else if (type === 'object') {
+    	return 0;
+    }
+    //check for undefined data types
+    else if (type === 'undefined') {
+    	return 0;
+    }
 }
