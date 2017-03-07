dcaa5b728232968c2f291b0a26a5c58dc0f6d457
Mariam Mghenyi <mchanya75@gmail.com>
2017-03-07 13:43:37 +0300

    solution: array-reduce

--- issue-009/solution.js
+++ issue-009/solution.js
@@ -1,4 +1,14 @@
 // implement your solution here
 function sumEven(a) {
-
+	//create new array, from given array, with only even numbers
+	var arrEven = [];
+	a.forEach(function(element) {
+		if (element%2 === 0){
+			arrEven.push(element);
+		}
+	});
+	var total = arrEven.reduce(function (accumulator, value){
+			return accumulator + value;
+	}, 0)
+	return total;
 }
